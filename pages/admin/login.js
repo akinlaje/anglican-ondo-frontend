58f1f5abc64c122af51012178dd70c897ce0631c
Olalekan Raheem <raheemolalekanusman84@gmail.com>
2022-01-06 12:17:59 +0100

    a

--- pages/admin/login.js
+++ pages/admin/login.js
@@ -6,84 +6,80 @@ import Spinner from '../../components/Spinner/Spinner';
 import axios from 'axios';
 import { FaUser as UserIcon } from 'react-icons/fa';
 
-const Login  = ({ setAdmin, setAuthToken, admin, apiBaseUrl }) => {
-	const [username, setUsername] = useState('');
-	const [password, setPassword] = useState('');
-	const [error, setError] = useState('');
-	const [loading, setLoading] = useState(false);
-	const [loggedIn, setLoggedIn] = useState(false)
+const Login = ({ setAdmin, setAuthToken, admin, apiBaseUrl }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
 
-	const router = useRouter();
+  const router = useRouter();
 
-	let returnUrl = router?.query?.returnUrl || '/admin/home'
+  let returnUrl = router?.query?.returnUrl || '/admin/home';
 
-	useEffect(() => {
-		if (admin && admin.id) {
-			router.push(returnUrl)
-		}
-	},)
+  useEffect(() => {
+    if (admin && admin.id) {
+      router.push(returnUrl);
+    }
+  });
 
-	const submit = async e => {
-		e.preventDefault();
-		setLoading(true);
-		const user = {
-			username,
-			password
-		}
+  const submit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const user = {
+      username,
+      password,
+    };
 
-		const { data } = await axios.post(apiBaseUrl + 'user/login', user, {
-			headers: {
-		    'Content-Type': 'application/json',
-		  },
-		});
-		const { token, user: admin, success, message } = data;
+    const { data } = await axios.post(apiBaseUrl + 'user/login/', user, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const { token, user: admin, success, message } = data;
 
-		if (!success) {
-			setError(message);
-			setLoading(false);
-			return
-		}
+    if (!success) {
+      setError(message);
+      setLoading(false);
+      return;
+    }
 
-		setLoading(false);
-		setLoggedIn(true)
-		setAdmin(admin);
-		setAuthToken(token)
-	}
+    setLoading(false);
+    setLoggedIn(true);
+    setAdmin(admin);
+    setAuthToken(token);
+  };
 
-	// email: raheemolalekanusman94@gmail.com
-	// username: creator
-	// password: creato?!r1
+  // email: raheemolalekanusman94@gmail.com
+  // username: creator
+  // password: creato?!r1
 
-	return (
-		<div className={styles.Container}>
-			<form className={styles.Form} onSubmit={submit}>
-				<h1 className={styles.Heading}>
-					<UserIcon size='50px' className={styles.HeadingIcon} />
-					Admin Login
-				</h1>
-				<label className={styles.Label}>
-					Username
-				</label>
-				<input 
-					className={styles.Input}
-					value={username}
-					onChange={e => setUsername(e.target.value)}
-				/>
-				<label className={styles.Label}>
-					Password
-				</label>
-				<input 
-					className={styles.Input}
-					value={password}
-					onChange={e => setPassword(e.target.value)}
-				/>
-				{error && <FormError error={error} />}
-				<button className={styles.SubmitButton} disabled={loading}>
-					{loading ? <Spinner /> : 'Login'}
-				</button>
-			</form>
-		</div>
-	)
-}
+  return (
+    <div className={styles.Container}>
+      <form className={styles.Form} onSubmit={submit}>
+        <h1 className={styles.Heading}>
+          <UserIcon size='50px' className={styles.HeadingIcon} />
+          Admin Login
+        </h1>
+        <label className={styles.Label}>Username</label>
+        <input
+          className={styles.Input}
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label className={styles.Label}>Password</label>
+        <input
+          className={styles.Input}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {error && <FormError error={error} />}
+        <button className={styles.SubmitButton} disabled={loading}>
+          {loading ? <Spinner /> : 'Login'}
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
