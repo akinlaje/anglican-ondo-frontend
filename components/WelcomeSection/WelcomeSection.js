e0d7d1d5e157d09a121511b0fc78cc2378a92ccc
Olalekan Raheem <raheemolalekanusman84@gmail.com>
2022-01-28 13:26:56 +0100

    last

--- components/WelcomeSection/WelcomeSection.js
+++ components/WelcomeSection/WelcomeSection.js
@@ -1,26 +1,35 @@
 import styles from './WelcomeSection.module.css';
 
 const WelcomeSection = () => {
-	return (
-		<section className={[styles.Section, styles.WelcomeSection].join(' ')}>
+  return (
+    <section className={[styles.Section, styles.WelcomeSection].join(' ')}>
       <h2 className={styles.SectionHeading}>WELCOME TO OUR OFFICIAL WEBSITE</h2>
       <p className={styles.WelcomeText}>
         <em>
-          You are most welcome to the official website of the Diocese of Ondo (Anglican Communion). We are in the age of information where communication has been so enhanced by technology such that information flies within a twinkle of an eye to every nook and cranny of the globe.
+          You are most welcome to the official website of the Diocese of Ondo
+          (Anglican Communion). We are in the age of information where
+          communication has been so enhanced by technology such that information
+          flies within a twinkle of an eye to every nook and cranny of the
+          globe.
         </em>
       </p>
       <p className={styles.WelcomeText}>
         <em>
-          Our Diocese, one of the foremost and oldest Dioceses in Nigeria, is not lagging behind in terms of using technology to promote the Gospel and disseminate information. On our website, you will get to know more about our Diocese, our Vision and Mission statements, our core values and other necessary and sufficient information you need to know. Furthermore, news updates, online radio broadcast, live streamed videos of events in the Diocese and our periodicals shall be showcased and readily updated on this unique website.
+          Our Diocese, one of the foremost and oldest Dioceses in Nigeria, is
+          not lagging behind in terms of using technology to promote the Gospel
+          and disseminate information. On our website, you will get to know more
+          about our Diocese, our Vision and Mission statements, our core values
+          and other necessary and sufficient information you need to know.
+          Furthermore, news updates, online radio broadcast, live streamed
+          videos of events in the Diocese and our periodicals shall be showcased
+          and readily updated on this unique website.
         </em>
       </p>
       <div>
-        <b>
-          + Stephen Adeniran Oni, PhD Mcasson   
-        </b>
+        <b>+ Rt. Revd. Stephen Adeniran Oni. PhD, Mcasson</b>
       </div>
     </section>
-	)
-}
+  );
+};
 
 export default WelcomeSection;
--- pages/about.js
+++ pages/about.js
@@ -2,79 +2,135 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import styles from '../styles/About.module.css';
-import { HiMenuAlt3 as MenuIcon } from 'react-icons/hi'
+import { HiMenuAlt3 as MenuIcon } from 'react-icons/hi';
 
 const About = () => {
-	return (
-		<>
-			<header className={styles.Header}>
+  return (
+    <>
+      <header className={styles.Header}>
         <Head>
           <title>About | Anglican Diocese of Ondo</title>
-          <meta name="description" content="About the Anglican diocese of Ondo" />
-          <link rel="icon" href="/images/diocese-logo.png" />
+          <meta
+            name='description'
+            content='About the Anglican diocese of Ondo'
+          />
+          <link rel='icon' href='/images/diocese-logo.png' />
         </Head>
-				<div className={styles.HeaderInner}>
-					<h1 className={styles.Heading}>About the Diocese</h1>
-					<p className={styles.HeadingText}>
-						Learn more about the Church - our services, history and our values
-					</p>
-				</div>
-			</header>
-			<section>
-				<div className={styles.AboutArticles}>
-					<div className={styles.AboutArticle}>
-						<div className={styles.ArticleInfo}>
-							<h2 className={styles.ContentHeading}>Our Vision</h2>
-							<p className={styles.Content}>
-								{`To be a leading Diocese in the Church of Nigeria (Anglican Communion)
+        <div className={styles.HeaderInner}>
+          <h1 className={styles.Heading}>About the Diocese</h1>
+          <p className={styles.HeadingText}>
+            Learn more about the Church - our services, history and our values
+          </p>
+        </div>
+      </header>
+      <section>
+        <div className={styles.AboutArticles}>
+          <div className={styles.AboutArticle}>
+            <div className={styles.ArticleInfo}>
+              <h2 className={styles.ContentHeading}>Our Vision</h2>
+              <p className={styles.Content}>
+                {`To be a leading Diocese in the Church of Nigeria (Anglican Communion)
 								in the proclamation of the Gospel and populating of God's Kingdom`}
-							</p>
-						</div>
-						<div className={styles.Image}>
-							<Image layout='fill' objectFit='cover' src='/images/church.jpeg' alt='Church'/>
-						</div>
-						<div className={styles.Bottom}>
-							<span>Read More</span>
-							<MenuIcon size='30px'color='#000' className={styles.MenuIcon} />
-						</div>
-					</div>
-					<div className={styles.AboutArticle}>
-						<div className={styles.ArticleInfo}>
-							<h2 className={styles.ContentHeading}>Our Mission</h2>
-							<p className={styles.Content}>
-								A &apos;Cruce Salus&apos; (safety of the cross) is our watchword and will continue to be 
-								our guide.
-							</p>
-							<p className={styles.Content}>
-								We shall point the way to the Cross of Christ; the source of our Salvation.
-							</p>
-							<p className={styles.Content}>
-								We shall through lip and life endeavour to proclaim our Risen Lord Jesus to every
-								 land and clime through the spread of the Good News.
-							</p>
-							<p className={styles.Content}>
-								We shall focus on Missions and Evangelism.
-							</p>
-							<p className={styles.Content}>
-								We shall motivate and inspire all to fulfill their God-given potentials.
-							</p>
-							<p className={styles.Content}>
-								We shall seek to lead people to our ultimate goal, i. e. to make heaven as they seek things
-								 that are above and not below.
-							</p>
-						</div>
-						<div className={styles.Image}>
-							<Image layout='fill' objectFit='cover' src='/images/church.jpeg' alt='Church'/>
-						</div>
-						<div className={styles.Bottom}>
-							<span>Read More</span>
-							<MenuIcon size='30px'color='#000' className={styles.MenuIcon} />
-						</div>
-					</div>
-				</div>
-			</section>
-		</>
-	)
-}
+              </p>
+            </div>
+            <div className={styles.Image}>
+              <Image
+                layout='fill'
+                objectFit='cover'
+                src='/images/church.jpeg'
+                alt='Church'
+              />
+            </div>
+            <div className={styles.Bottom}>
+              <span>Read More</span>
+              <MenuIcon size='30px' color='#000' className={styles.MenuIcon} />
+            </div>
+          </div>
+          <div className={styles.AboutArticle}>
+            <div className={styles.ArticleInfo}>
+              <h2 className={styles.ContentHeading}>Our Mission</h2>
+              <p className={styles.Content}>
+                A &apos;Cruce Salus&apos; (safety of the cross) is our watchword
+                and will continue to be our guide.
+              </p>
+              <p className={styles.Content}>
+                We shall point the way to the Cross of Christ; the source of our
+                Salvation.
+              </p>
+              <p className={styles.Content}>
+                We shall through lip and life endeavour to proclaim our Risen
+                Lord Jesus to every land and clime through the spread of the
+                Good News.
+              </p>
+              <p className={styles.Content}>
+                We shall focus on Missions and Evangelism.
+              </p>
+              <p className={styles.Content}>
+                We shall motivate and inspire all to fulfill their God-given
+                potentials.
+              </p>
+              <p className={styles.Content}>
+                We shall seek to lead people to our ultimate goal, i. e. to make
+                heaven as they seek things that are above and not below.
+              </p>
+            </div>
+            <div className={styles.Image}>
+              <Image
+                layout='fill'
+                objectFit='cover'
+                src='/images/church.jpeg'
+                alt='Church'
+              />
+            </div>
+            <div className={styles.Bottom}>
+              <span>Read More</span>
+              <MenuIcon size='30px' color='#000' className={styles.MenuIcon} />
+            </div>
+          </div>
+          <div className={styles.AboutArticle}>
+            <div className={styles.ArticleInfo}>
+              <h2 className={styles.ContentHeading}>Our Anthem</h2>
+              <p className={styles.content}>DIOCESAN ANTHEM</p>
+              <p className={styles.Content}>
+                1. A Cruce Salus; Agbelebu <br />
+                Kristi fun ‘gbala wa <br />
+                Wa lori asia wa <br />
+                Ni Ondo Daosis’ <br />
+                Ni Odun 1952 <br />
+                L’Oluwa da sile <br />
+                Gegebi Ondo-Benin <br />
+                Anglikan ‘Daosis
+              </p>
+              <p className={styles.Content}>
+                2. Ise wa ni lati kede
+                <br />
+                Jesu Olugbala
+                <br />
+                Fun gbogbo Orile-Ede
+                <br />
+                Ihin igbala wa
+                <br />
+                E ho f’ayo k’inu wa dun
+                <br />
+                Igbala wa ti de
+                <br />
+                Sun mo ju ’gba t’a ko gbagbo
+                <br />E ho Halleluyah!
+              </p>
+            </div>
+            <div className={styles.Image}>
+              <Image
+                layout='fill'
+                objectFit='cover'
+                src='/images/diocese-logo.png'
+                alt='Church'
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default About;
